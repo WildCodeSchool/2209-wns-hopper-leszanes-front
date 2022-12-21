0b4275f91547cb9fd031bb4223ee50de34295654
Maurin Célian <celianmaurin@hotmail.com>
2022-12-21 17:24:11 +0100

    Button profile header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,16 @@
 import zanelogo from "../../assets/images/zanelogo.png";
 import styles from "./Header.module.scss";
+import { Navbar } from "../navbar/Navbar";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
       <img src={zanelogo} alt="" className={styles.logo} />
       <h1>Ztransfert</h1>
+      <Navbar />
+      <button className={styles.profileButton} type="button">
+        Icon
+      </button>
     </header>
   );
 };
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import styles from "./Navbar.module.scss";
+
+export const Navbar = () => {
+  return (
+    <nav className={styles.header}>
+      <h1>Test</h1>
+    </nav>
+  );
+};
