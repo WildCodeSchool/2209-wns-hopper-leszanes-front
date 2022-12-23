c27cd5c552cfeadda5b6a0ab1aface9c0354f296
Yann Birba <birbayann@gmail.com>
2022-12-23 17:38:29 +0100

    fix: e2e test

--- src/test/e2e/ztransfer.test.ts
+++ src/test/e2e/ztransfer.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { afterAll, beforeAll, describe, test } from "vitest";
 import { preview } from "vite";
 import type { PreviewServer } from "vite";
@@ -6,35 +7,51 @@ import type { Browser, Page } from "playwright";
 import { expect } from "@playwright/test";
 
 describe("Router", () => {
+  // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error, @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   let server: PreviewServer;
   let browser: Browser;
   let page: Page;
 
   beforeAll(async () => {
-    server = await preview({ preview: { port: 3000 } });
-    browser = await chromium.launch();
-    page = await browser.newPage();
-  });
+    server = await preview({
+      preview: {
+        port: 3000,
+      },
+    });
 
+    browser = await chromium.launch();
+  });
   afterAll(async () => {
     await browser.close();
-    await new Promise<void>((resolve, reject) => {
-      server.httpServer.close((error) => (error ? reject(error) : resolve()));
-    });
   });
+  beforeEach(async () => {
+    page = await browser.newPage();
+  });
+  afterEach(async () => {
+    await page.close();
+  });
+
   test("go to the contact page when user click on the contact link", async () => {
     await page.goto("http://localhost:3000");
-    await page.getByRole("link", { name: "Contacts" }).click();
+    await page.getByTestId("contacts").click();
     await expect(page).toHaveURL("http://localhost:3000/contacts");
-  });
-  test("go to the profile page when user click on the profile link", async () => {
-    await page.goto("http://localhost:3000");
-    await page.getByRole("link", { name: "Profile" }).click();
-    await expect(page).toHaveURL("http://localhost:3000/profile");
   });
   test("go to the files page when user click on the files link", async () => {
     await page.goto("http://localhost:3000");
-    await page.getByRole("link", { name: "Fichiers" }).click();
+    await page.getByTestId("files").click();
     await expect(page).toHaveURL("http://localhost:3000/files");
+  });
+  test("go to the upload page when user click on the upload link", async () => {
+    await page.goto("http://localhost:3000");
+    await page.getByTestId("upload").click();
+    await expect(page).toHaveURL("http://localhost:3000/upload");
+  });
+
+  test("go to the home page when user click on the home link", async () => {
+    await page.goto("http://localhost:3000");
+    await page.getByTestId("contacts").click();
+    await page.getByTestId("home").click();
+    await expect(page).toHaveURL("http://localhost:3000");
   });
 });
