67630a62d04a01559b2fc7d3b60f0015a3e5af6f
Yann Birba <birbayann@gmail.com>
2023-06-23 14:07:00 +0200

    fix: after merging

--- src/hooks/useDownloadAllTransferFiles.ts
+++ src/hooks/useDownloadAllTransferFiles.ts
@@ -4,7 +4,9 @@ import { File } from "../types/File";
 export const useDownloadAllTransferFiles = () => {
   const { download } = useFiles();
   const downloadAllFiles = async (fileList: File[]) => {
-    const files = fileList.map((f) => f.name);
+    const files = fileList.map((f) => {
+      return { fileName: f.fileName, name: f.name };
+    });
     await download(files, fileList);
   };
 
--- src/types/File.ts
+++ src/types/File.ts
@@ -3,6 +3,7 @@ import { Transfer } from "./Transfer";
 export type File = {
   id: number;
   name: string;
+  fileName: string;
   size: number;
   type: string;
   createdAt: string;
